ca9bac0f7bb3686596775626ec257cded3843150
DevMcryYu <30391572+DevMcryYu@users.noreply.github.com>
2017-10-29 22:14:07 +0800

    Add files via upload

---
+++ Head.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+
+/*定义结构体*/
+struct Student
+{
+	int Rank;
+	char ID[10];
+	char Name[20];
+	float Mark1;
+	float Mark2;
+	float Mark3;
+	float Mark4;
+	float Average;
+	
+};
